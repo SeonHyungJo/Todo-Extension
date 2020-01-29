794bd716e32a3d7f3f9aedbed319a11d97cae38a
SeonHyungJo <seonhyung.jo@gmail.com>
2020-01-29 20:50:23 +0900

    📝Create Request.getRepoId & export Request

--- src/github-api/request.ts
+++ src/github-api/request.ts
@@ -1,1 +1,20 @@
-// export const requestObj = () => { };
+import { Config } from '@src/model';
+
+// import * as ISSUE_API from '@githubApi/issue';
+// import * as LABEL_API from '@githubApi/label';
+import * as REPO_API from '@githubApi/repo';
+
+const GITHUB_URL = process.env.GITHUB_URL;
+const TOKEN = process.env.TOKEN;
+
+export const getRepoId = (config: Config) => {
+  return {
+    url: GITHUB_URL,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${config.token || TOKEN}`,
+    },
+    body: REPO_API.getRepoId(config),
+  };
+};
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,23 @@
-import { GraphQLClient } from 'graphql-request';
-import { ISSUE_API } from '@githubApi/index';
 import { Config } from '@model/index';
+
+import { GraphQLClient } from 'graphql-request';
+import { getRepoId } from '@githubApi/index';
+
+import { ajax } from 'rxjs/ajax';
+
+const token = process.env.TOKEN || '';
 
 const config: Config = {
   repositoryName: 'test-github-api',
   owner: 'seonhyungjo',
-  token: '',
+  token: token,
   repositoryId: '',
 };
 
-const getIssueList = async () => {
-  const endpoint: string = 'https://api.github.com/graphql';
-  const query: string = ISSUE_API.getIssueQuery(config);
-  const graphQLClient: GraphQLClient = new GraphQLClient(endpoint, {
-    headers: {
-      Authorization: `Bearer ${process.env.TOKEN}`,
-    },
-  });
-  const data = await graphQLClient.request(query);
-  return data;
-};
-
-console.log(getIssueList());
+// ajax(getRepoId(config)).pipe(
+//   map(response => console.log('response: ', response)),
+//   catchError(error => {
+//     console.log('error: ', error);
+//     return of(error);
+//   }),
+// );
