ab81d5cca399d805067ef9b616851dc705d1177d
SeonHyungJo <seonhyung.jo@gmail.com>
2020-03-25 01:20:16 +0900

    📝 Create Test getRepoId Epic

--- src/redux/auth.ts
+++ src/redux/auth.ts
@@ -1,8 +1,10 @@
 import { Observable } from 'rxjs';
+import { ajax, AjaxResponse } from 'rxjs/ajax';
 import { Action } from 'redux';
 import { ofType, combineEpics } from 'redux-observable';
-import { mapTo } from 'rxjs/operators';
+import { mapTo, mergeMap, map } from 'rxjs/operators';
 import { createAction } from 'redux-actions';
+import { getRepoId } from '@/githubApi/repo';
 
 // payload interface
 export interface IState {
@@ -12,10 +14,12 @@ export interface IState {
   repoID: string;
 }
 
-type IUserLoginParam = Omit<IState, 'repoID'>;
+export type IUserLoginParam = Omit<IState, 'repoID'>;
 
 // type
 export const USER_LOGIN = 'auth/USER_LOGIN';
+export const USER_LOGIN_FAIL = 'auth/USER_LOGIN_FAIL';
+
 export const USER_LOGIN_ASYNC = 'auth/USER_LOGIN_ASYNC';
 export const USER_LOGOUT = 'auth/USER_LOGOUT';
 
@@ -23,19 +27,44 @@ export const USER_LOGOUT = 'auth/USER_LOGOUT';
 // epic
 // TODO feature: 로그인|token확인|RepoId 받아오기
 export const userLogin = createAction(
-  USER_LOGIN,
+  USER_LOGIN_ASYNC,
   ({ repoName, token, owner }: IUserLoginParam) => ({
     repoName,
     token,
     owner,
-    repoID: 'testRepoID',
   }),
 );
 
 export const userLoginEpic = (
   action$: Observable<Action>,
 ): Observable<Action> => {
-  return action$.pipe(ofType(USER_LOGIN_ASYNC), mapTo({ type: USER_LOGIN }));
+  return action$.pipe(
+    ofType(USER_LOGIN_ASYNC),
+    mergeMap(action => {
+      console.log('userLoginEpic ===> ', action);
+      return ajax
+        .post(
+          process.env.END_POINT || '',
+          {
+            query: getRepoId({
+              repoName: 'test-github-api',
+              owner: 'seonhyungjo',
+              token: process.env.TOKEN || '',
+            }),
+          },
+          {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${process.env.TOKEN}`,
+          },
+        )
+        .pipe(
+          map(({ response: { data } }: AjaxResponse) => ({
+            type: USER_LOGIN,
+            payload: data,
+          })),
+        );
+    }),
+  );
 };
 
 // TODO feature: 로그아웃
@@ -50,8 +79,13 @@ const initialState: IState = {
 
 //reducer
 export function authReducer(state = initialState, action: any): IState {
+  console.log('Auth Reducer ', action);
   switch (action.type) {
     case USER_LOGIN:
+      return {
+        ...action.payload,
+      };
+    case USER_LOGIN_FAIL:
       return {
         ...action.payload,
       };
