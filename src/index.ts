41aea561c9131588c708ca1c4dbe2295b2a47d0f
SeonHyungJo <seonhyung.jo@gmail.com>
2020-04-20 23:53:48 +0900

    📝 Config Page 화면구성 및 Check 기능 추가

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import configureStore from '@/redux';
 import App from '@/components/App';
 
+import './reset.css';
+import './style.css';
+
 const store = configureStore();
 
 new App(document.getElementById('app') || document.body, store);
